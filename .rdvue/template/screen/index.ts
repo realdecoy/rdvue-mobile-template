4423a247decc37294f33f3b98c6b6fbbbad6db19
Anesia Smith <asmith@realdecoy.com>
2022-07-19 14:37:09 -0500

    Screen Template changes

---
+++ .rdvue/template/screen/index.ts
@@ -0,0 +1,1 @@
+export { default } from "./__SCREEN__KEBAB__";
---
+++ .rdvue/template/screen/screen.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { View } from "react-native";
+import styles from "./__SCREEN__.styles";
+
+interface Props {
+  value: string;
+}
+
+interface State {
+  stateValue: string;
+}
+
+class __SCREEN__ extends React.Component<Props, State> {
+  // --------------------------------------------------------------------------
+  // Constructer
+  // --------------------------------------------------------------------------
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      stateValue: "",
+    };
+  }
+
+  // --------------------------------------------------------------------------
+  // Methods
+  // --------------------------------------------------------------------------
+
+  // --------------------------------------------------------------------------
+  // [Lifecycle] Methods
+  // --------------------------------------------------------------------------
+  componentDidMount() {}
+
+  // --------------------------------------------------------------------------
+  // Render Method
+  // --------------------------------------------------------------------------
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.heading}>__SCREEN__ Screen</Text>
+        <Text>Value is {this.props.value}</Text>
+      </View>
+    );
+  }
+}
+
+export default __SCREEN__;
