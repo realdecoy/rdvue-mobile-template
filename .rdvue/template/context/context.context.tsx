ac5927b6738c14e7aadf68982f9b0c09f4becdc0
Davane Davis <ddavis@madmobile.com>
2023-01-23 14:59:18 -0500

    added camel case to the context prop

--- .rdvue/template/context/context.context.tsx
+++ .rdvue/template/context/context.context.tsx
@@ -42,7 +42,7 @@ class __CONTEXT__ContextProvider extends React.Component<Props, __CONTEXT__Conte
 const with__CONTEXT__ = (Component: any) => (props: any) =>
   (
     <__CONTEXT__Consumer>
-      {(state) => <Component {...props} __CONTEXT__={state} />}
+      {(state) => <Component {...props} __CONTEXT_CAMEL__={state} />}
     </__CONTEXT__Consumer>
   );
 
