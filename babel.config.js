22b378cf3bbcb6b29d0bb482ce58f98e2d1dccf8
Tyrone <28680107+ttaylor92@users.noreply.github.com>
2024-01-09 15:41:17 -0500

    added config files

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,16 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+            extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+            alias: {
+                '@': './src',
+            },
+        },
+      ],
+    ],
   };
 };
